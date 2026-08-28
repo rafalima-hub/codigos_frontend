aee54bc5c34f47f29f90cfc1477a1a06da0244f2
Rafaela Lima <rafasouzalima.rsl@gmail.com>
2026-08-28 20:25:23 -0300

    2026.08.28.2

---
+++ primeiros_passos_2026/js/scripts.js
@@ -0,0 +1,12 @@
+/* JavaScript do tamplate */
+
+if (pageName) {
+    console.log(pageName)
+}
+console.log("Ajuste o JavaScript dess página!")
+
+const myLogo = document.getElementsById('myLogo')
+// console.log(myLogo)
+myLogo.addEventListener("clikc", function () {
+    alert("Por que clicou aqui?")
+})
