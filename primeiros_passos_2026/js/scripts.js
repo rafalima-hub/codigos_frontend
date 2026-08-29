a6ac0b1df115cf3012ef0be71b6b8c1affb594d8
Rafaela Lima <rafasouzalima.rsl@gmail.com>
2026-08-28 21:13:19 -0300

    2026.08.28.03

--- primeiros_passos_2026/js/scripts.js
+++ primeiros_passos_2026/js/scripts.js
@@ -1,12 +1,35 @@
-/* JavaScript do tamplate */
+/* 
+js/scripts.js
+JavaScript do tamplate */
 
-if (pageName) {
+/* Recebe uma variável do HTML */ 
+if (typeof pageName === undefined) {
+    console.log("Ajuste o JavaScript dess página!")
+} else {
     console.log(pageName)
 }
-console.log("Ajuste o JavaScript dess página!")
 
-const myLogo = document.getElementsById('myLogo')
+
+const myLogo = document.getElementById('myLogo')
 // console.log(myLogo)
 myLogo.addEventListener("clikc", function () {
     alert("Por que clicou aqui?")
+});
+
+/********************
+ * Aviso de cookies *
+ ********************/
+
+const cookieBanner = document.getElementById("cookie-banner");
+const cookieAccept = document.getElementById("cookie-accept");
+
+// Verifica se o usuário já aceitou os cookies
+if (localStorage.getItem("cookiesAccepted") === "true") {
+    cookieBanner.style.display = "none";
+}
+
+// Botão "Aceitar"
+cookieAccept.addEventListener("click", function () {
+    localStorage.setItem("cookiesAccepted", "true");
+    cookieBanner.style.display = "none";
 });
